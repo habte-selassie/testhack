403e557c9135819b0064b497595e4dc70efae296
Habte Selassie <habtuchelsea1000@gmail.com>
2024-08-07 07:57:19 +0300

    first ui test

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import StakingYeildFarming from './StakingAndFarming';
+import GovernanceAndVoting from './Governance';
+
+
 
 ReactDOM.render(
   <App />,
